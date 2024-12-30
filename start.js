337204e667b3b80848c819ed37d5dc6a05f3d7ff
TopsterTX <bbs.100@yandex.ru>
2024-12-31 02:39:44 +0300

    [feature] change the start file

--- start.js
+++ start.js
@@ -3,6 +3,6 @@ require('dotenv').config()
 const cli = require('next/dist/cli/next-start')
 
 cli.nextStart({
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 3001,
   hostname: process.env.HOST || 'localhost',
 })
